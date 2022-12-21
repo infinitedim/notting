3519d2d4eb1b19513c192bf633e565c6802aa95f
unknown <saputradimass.boediluhur@gmail.com>
2022-12-21 16:05:23 +0700

    refactor some code

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,20 +2,20 @@ export type Note = {
   id: string;
 } & NoteData;
 
-export interface RawNote {
-  id: string;
+export interface NoteData {
+  title: string | undefined;
+  markdown: string | undefined;
+  tags: Tag[];
 }
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
 
 export interface RawNoteData {
   title: string | undefined;
   markdown: string | undefined;
   tagIds: string;
-}
-
-export interface NoteData {
-  title: string | undefined;
-  markdown: string | undefined;
-  tags: Tag[];
 }
 
 export interface Tag {
@@ -25,4 +25,12 @@ export interface Tag {
 
 export interface NoteFormProps {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+}
+
+export interface NewNoteProps {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 }
